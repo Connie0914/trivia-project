b6a789ef509985ac00eb7c9a2780a49420aec1d2
ユウト <jiyeyouren523@gmail.com>
2021-11-02 07:23:23 +0900

    add base broadcastlist

---
+++ src/components/BroadCastList/index.tsx
@@ -0,0 +1,8 @@
+export const BroadCastList = (): JSX.Element => {
+  return (
+    <ul>
+      <li>hoge1</li>
+      <li>hoge2</li>
+    </ul>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,12 @@ import { NextPage } from 'next';
 import Image from 'next/image';
 import { DefaultLayout } from '../layouts/defaultLayout';
 import { Button } from '../components/Button';
+import { BroadCastList } from '../components/BroadCastList';
 
 const Index: NextPage = () => {
   return (
     <DefaultLayout>
-      <p>Contents</p>
+      <BroadCastList />
     </DefaultLayout>
   );
 };
