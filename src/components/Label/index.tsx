b55061c68bd26b47448141d8e88dbc7aa7b6e569
ユウト <jiyeyouren523@gmail.com>
2021-11-18 12:42:30 +0900

    🎨 レビューいただいた箇所の修正をしました

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -1,10 +1,30 @@
 export type Props = {
-  className?: string;
-  labelText: '放送中' | '放送前・エンジビア募集中' | '放送済み';
+  type: 'broadcasting' | 'beforeBroadcasting' | 'afterBroadcasting';
 };
 
 export const Label = (props: Props): JSX.Element => {
-  const { className, labelText } = props;
+  const { type } = props;
 
-  return <span className={className}>{labelText}</span>;
+  if (type === 'beforeBroadcasting') {
+    return (
+      <div className="text-sm text-red-600 py-1 px-3 bg-yellow-100 rounded-2xl">
+        <span>放送前・エンジビア募集</span>
+      </div>
+    );
+  }
+  if (type === 'broadcasting') {
+    return (
+      <div className="text-green-600 px-3 bg-green-200 rounded-2xl">
+        <span>放送中</span>
+      </div>
+    );
+  }
+  if (type === 'afterBroadcasting') {
+    return (
+      <div className="text-sm py-1 px-3 bg-gray-200 rounded-2xl">
+        <span>放送後</span>
+      </div>
+    );
+  }
+  return <div></div>;
 };
